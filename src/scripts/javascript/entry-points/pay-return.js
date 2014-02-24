75fb1c6e2f64f5cacb020a09b613a307824728d6
Marlon Santos Constante <marlon.constante@gmail.com>
2014-02-24 18:45:06 -0300

    - Removendo nice-scroll do mobile - Adicionando tela de retorno de pagamento

--- src/scripts/javascript/entry-points/pay-return.js
+++ src/scripts/javascript/entry-points/pay-return.js
@@ -1,9 +1,6 @@
 var $ = require("../../../libs/jquery/jquery");
 var niceScroll = require("../nice-scroll");
-var responsiveSlides = require("../responsive-slides");
 
 $(document).ready(function () {
     niceScroll($, "html");
-    responsiveSlides($, ".banner > .products > ul");
 });
-
