c6f663169043fd2d5e6c1c727a7f00c186e10af7
Marlon Santos Constante <marlon.constante@gmail.com>
2014-03-24 14:22:22 -0300

    Refactor no componente do pag-seguro

--- src/scripts/javascript/payment-options.js
+++ src/scripts/javascript/payment-options.js
@@ -1,14 +1,27 @@
+var accounting = require("../../libs/accounting/accounting");
+
 module.exports = function ($) {
     "use strict";
 
+    accounting.settings.currency.symbol = "R$ ";
+    accounting.settings.currency.decimal = ",";
+    accounting.settings.currency.thousand = ".";
+
     window.addEventListener("polymer-ready", function () {
+        var pagSeguro = $("dl-button-pag-seguro")[0];
         var $options = $((document.querySelector("dl-options") || {}).impl).find("input[type=radio]");
-        $options.on("click", function () {
-            var $option = $(this);
-            $("dl-button-pag-seguro")[0].code = $option.data("value");
-            $(".price").text($option.data("additionalDescription"));
-        });
-        $options.first().trigger("click");
+        if ($options.length) {
+            $options.on("click", function () {
+                var $option = $(this);
+                pagSeguro.partner = $option.data("pagSeguroPartner");
+                pagSeguro.description = $option.data("pagSeguroDescription");
+                pagSeguro.amount = $option.data("pagSeguroAmount");
+                $(".price").text(accounting.formatMoney(pagSeguro.amount));
+            });
+            $options.first().trigger("click");
+        } else {
+            $(".price").text(accounting.formatMoney(pagSeguro.amount));
+        }
     });
 
 };
